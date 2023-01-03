27c68697ad50152d05071716030dc45a65ab2f6e
Onsiter <tuannmd@fsoft.com.vn>
2023-01-03 13:45:41 +0900

    add training model codes, model_card

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,13 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import logging
+
+
+logging.basicConfig(filename='logging.log',
+                    level=logging.INFO,
+                    filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +27,14 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    logging.info("Start training RandomForestClassifier")
+
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+
+    clf.fit(X_train, y_train)
+    logging.info("********* Finish training! ***********")
+
+    return clf
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +73,46 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+
+    return preds
+
+
+def compute_slices(df, feature, y, preds):
+    """
+    Compute the performance on slices for a given categorical feature
+    ------
+    df: 
+        test dataframe
+    feature:
+        feature on which to perform the slices
+    y : np.array
+        True labels
+    preds : np.array
+        Predicted labels
+    Returns
+    ------
+    Dataframe with
+        n_samples: integer - number of data samples in the slice
+        precision : float
+        recall : float
+        fbeta : float
+    row corresponding to each of the unique values taken by the feature (slice)
+    """    
+    slice_values = df[feature].unique().tolist()
+    res_df = pd.DataFrame(index=slice_values, 
+                          columns=['feature','n_samples','precision', 'recall', 'fbeta'])
+    for val in slice_values:
+        slice_y = y[df[feature]==val]
+        slice_preds = preds[df[feature]==val]
+        precision, recall, fbeta = compute_model_metrics(slice_y, slice_preds)
+        
+        res_df.at[val, 'feature'] = feature
+        res_df.at[val, 'n_samples'] = len(slice_y)
+        res_df.at[val, 'precision'] = precision
+        res_df.at[val, 'recall'] = recall
+        res_df.at[val, 'fbeta'] = fbeta
+
+    res_df.reset_index(names='feature value', inplace=True)
+
+    return res_df
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,28 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pickle, os
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference, compute_slices
+import logging
 
-# Add the necessary imports for the starter code.
+
+logging.basicConfig(filename='loggin.log',
+                    level=logging.INFO,
+                    filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 # Add code to load in the data.
+datapath = "../data/census.csv"
+data = pd.read_csv(datapath)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split( data, 
+                                test_size=0.20, 
+                                random_state=2023, 
+                                stratify=data['salary']
+                                )
 
 cat_features = [
     "workclass",
@@ -24,5 +39,36 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
+
+savepath = '../model'
+filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+# save model  to disk in ./model folder
+pickle.dump(model, open(os.path.join(savepath,filename[0]), 'wb'))
+pickle.dump(encoder, open(os.path.join(savepath,filename[1]), 'wb'))
+pickle.dump(lb, open(os.path.join(savepath,filename[2]), 'wb'))
+logging.info(f"Model saved to disk: {savepath}")
+logging.info(f"Training params: {model.get_params()}")
+
+# evaluate trained model on test set
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+logging.info(f"precision:{precision:.3f}, recall:{recall:.3f}, fbeta:{fbeta:.3f}")
+
+
+# Compute performance on slices for categorical features
+for feature in cat_features:
+    performance_df = compute_slices(test, feature, y_test, preds)
+    logging.info(f"Performance on slice {feature}")
+    logging.info(performance_df)
