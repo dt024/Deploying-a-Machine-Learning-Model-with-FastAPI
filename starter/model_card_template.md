4589ee01e80b2936125cd47362d8b672a939c4c9
Onsiter <tuannmd@fsoft.com.vn>
2023-01-03 13:52:25 +0900

    add model_test, train_model, and model card

--- starter/model_card_template.md
+++ starter/model_card_template.md
@@ -3,16 +3,43 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+Prediction task is to classify whether a person makes over 50K a year. In this project RandomForestClassifier is used within scikit-learn 1.2.0. Parameters used are:
+
+- learning_rate: 1.0
+- max_depth: 2
+- min_samples_split: 2
+- random_state: 0 
+- min_samples_leaf: 1
+- bootstrap: True
+- n_estimators: 100 
+
+Model is saved as a pickle file in the model folder. All training steps and metrics are logged in the file "logging.log".
 
 ## Intended Use
+This model can be used to predict the salary level of an individual.
 
 ## Training Data
+The Census Income Dataset was obtained from the UCI Machine Learning Repository (https://archive.ics.uci.edu/ml/datasets/census+income). The original csv file has 32.561 rows and 15 columns including binary target label "salary", 8 categorical features and 6 numerical features. Please check the above link for more detail.
+Target label "salary" has two classes ('<=50K', '>50K') with a ratio of circa 75% / 25% which is highly imbalance.
+A simple data cleansing was performed on the original dataset to remove leading and trailing whitespaces. See EDA_cleaning.ipynb notebook for cleansing step.
+
+The dataset was split into a train set and a test set using an 80-20 split with salary-based stratification.
+A label binarizer was applied to the target and a One Hot Encoder was applied to the category features to use the data for training.
 
 ## Evaluation Data
+20% of the dataset was used for model evaluation.
+Same pre-processing as Training Data is applied to Evaluation Data.
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+The classification performance is evaluated using precision, recall and fbeta metrics.
+
+The model achieves below scores using the test set:
+- precision:0.759
+- recall:0.643
+- fbeta:0.696
 
 ## Ethical Considerations
+The dataset shouldn't be regarded as an accurate representation of the pay distribution or as a basis for assuming the salary level of particular population categories.
 
 ## Caveats and Recommendations
+The 1994 Census database was mined for information. As an out-of-date sample, the dataset is insufficient for use as a statistical representation of the population. It is advised to utilize the dataset to train on ML classification or other related issues.
---
+++ test_model.py
@@ -0,0 +1,149 @@
+import pytest, os, logging, pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.exceptions import NotFittedError
+
+from starter.starter.ml.model import inference, compute_model_metrics
+from starter.starter.ml.data import process_data
+
+
+"""
+Fixture - The test functions will 
+use the return of data() as an argument
+"""
+@pytest.fixture(scope="module")
+def data():
+    # code to load in the data.
+    datapath = "./starter/data/census.csv"
+    return pd.read_csv(datapath)
+
+
+@pytest.fixture(scope="module")
+def path():
+    return "./starter/data/census.csv"
+
+
+@pytest.fixture(scope="module")
+def features():
+    """
+    Fixture - return the categorical features as argument
+    """
+    cat_features = [    "workclass",
+                        "education",
+                        "marital-status",
+                        "occupation",
+                        "relationship",
+                        "race",
+                        "sex",
+                        "native-country"]
+    return cat_features
+
+
+@pytest.fixture(scope="module")
+def train_dataset(data, features):
+    """
+    Fixture - returns train dataset to be used for model testing
+    """
+    train, test = train_test_split( data, 
+                                test_size=0.20, 
+                                random_state=10, 
+                                stratify=data['salary']
+                                )
+    X_train, y_train, encoder, lb = process_data(
+                                            train,
+                                            categorical_features=features,
+                                            label="salary",
+                                            training=True
+                                        )
+    return X_train, y_train
+
+
+def test_import_data(path):
+    """
+    Test presence and shape of dataset file
+    """
+    try:
+        df = pd.read_csv(path)
+
+    except FileNotFoundError as err:
+        logging.error("File not found")
+        raise err
+
+    # Check the df shape
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+
+    except AssertionError as err:
+        logging.error(
+        "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
+
+
+def test_features(data, features):
+    """
+    Check that categorical features are in dataset
+    """
+    try:
+        assert sorted(set(data.columns).intersection(features)) == sorted(features)
+    except AssertionError as err:
+        logging.error(
+        "Features are missing in the data columns")
+        raise err
+
+
+def test_is_model():
+    """
+    Check saved model is present
+    """
+    path = "./starter/model/trained_model.pkl"
+    if os.path.isfile(path):
+        try:
+            _ = pickle.load(open(path, 'rb'))
+        except Exception as err:
+            logging.error(
+            "Saved model not found!")
+            raise err
+    else:
+        pass
+
+
+def test_inference(train_dataset):
+    """
+    Check inference function
+    """
+    X_train, y_train = train_dataset
+
+    path = "./starter/model/trained_model.pkl"
+    if os.path.isfile(path):
+        model = pickle.load(open(path, 'rb'))
+
+        try:
+            preds = inference(model, X_train)
+        except Exception as err:
+            logging.error(
+            "Inference cannot be performed on saved model and train data")
+            raise err
+    else:
+        pass
+
+
+def test_compute_model_metrics(train_dataset):
+    """
+    Check calculation of performance metrics function
+    """
+    X_train, y_train = train_dataset
+
+    path = "./starter/model/trained_model.pkl"
+    if os.path.isfile(path):
+        model = pickle.load(open(path, 'rb'))
+        preds = inference(model, X_train)
+
+        try:
+            precision, recall, fbeta = compute_model_metrics(y_train, preds)
+        except Exception as err:
+            logging.error(
+            "Performance metrics cannot be calculated on train data")
+            raise err
+    else:
+        pass
